010c88adca09e02aa9e138c7e85b8afe71c96681
Piotr Ferenc <ferenc.piotr@outlook.com>
2025-03-20 08:39:10 +0100

    Add unit tests project for Hypertext.Bootstrap5
    Introduce GridTests with a basic rendering test for Bootstrap 5 grid structure. Configure the test project with .NET 9.0, xUnit, and necessary dependencies to enable testing and code coverage.

---
+++ Hypertext.Bootstrap5.Tests/GridTests.cs
@@ -0,0 +1,19 @@
+﻿using System.Xml.Serialization;
+
+namespace Hypertext.Bootstrap5.Tests;
+
+public class GridTests
+{
+    [Fact]
+    public void Test()
+    {
+        var grid = Grid.Container(
+            Grid.Row(
+                Grid.Column(Html.Text("123"))
+            )
+        );
+        var tt = grid.Render();
+
+        Assert.Equal("<div class=\"container\"><div class=\"row\"><div class=\"col\">123</div></div></div>", tt);
+    }
+}
