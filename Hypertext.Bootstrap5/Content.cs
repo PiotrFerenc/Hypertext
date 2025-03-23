e99b80630da7157dc69e11a83a4e341b7c67c350
Piotr Ferenc <ferenc.piotr@outlook.com>
2025-03-23 09:30:24 +0100

    Refactor TableBuilder<T> to a separate file.
    Moved the `TableBuilder<T>` class from `Content.cs` to a new `TableBuilder.cs` file to improve code organization and maintainability. No functional changes were made during this refactor.

--- Hypertext.Bootstrap5/Content.cs
+++ Hypertext.Bootstrap5/Content.cs
@@ -41,34 +41,3 @@ public static class Content
         public static HtmlElement Dark(params HtmlElement[] rows) => Html.Table(rows).WithClass(BootstrapConstants.TableDarkClass);
     }
 }
-
-public class TableBuilder<T>
-{
-    private Dictionary<string, Func<T, HtmlElement>> _rows = new();
-
-    public delegate HtmlElement RowBuilder(params HtmlElement[] rows);
-
-    public TableBuilder<T> WithRow(string name, Func<T, HtmlElement> row)
-    {
-        _rows.Add(name, row);
-        return this;
-    }
-
-    public HtmlElement Build(List<T> items, RowBuilder rowBuilder)
-    {
-        var head = Html.Thead(Html.Tr(_rows.Select(x => Html.Th(Html.Text(x.Key))).ToArray()));
-        var body = new List<HtmlElement>();
-        foreach (var item in items)
-        {
-            foreach (var row in _rows)
-            {
-                var td = Html.Td(row.Value(item).Render());
-                var tr = Html.Tr(td);
-                body.Add(tr);
-            }
-        }
-
-        var rows = new[] { head, Html.Tbody(body.ToArray()) };
-        return rowBuilder(rows);
-    }
-}
